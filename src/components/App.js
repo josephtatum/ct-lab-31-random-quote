3e998723f19c2a30d0a6b8a597a739ada1f82c4a
Joseph Tatum <josephtatum@gmail.com>
2020-02-03 14:28:09 -0800

    re-wrote app js file to use class component and updated queen function to set state as well as load a queen after compnent mounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import Button from './Button';
 import Result from './Result';
 
-const getRandomQueen = () => {
+export default class App extends Component {
 
-  return fetch('http://www.nokeynoshade.party/api/queens/')
-    .then(res => res.json())
-    .then(res => res[Math.floor(Math.random() * res.length)])
-    .then(res => console.log(res));
-};
+  state = {
+    queen: {}
+  }
 
-export default function App() {
+  getRandomQueen = () => {
 
-  return (
-    <>
-      <Button onClick={getRandomQueen}/>
-      <Result />
-    </>
-  );
+    return fetch('http://www.nokeynoshade.party/api/queens/')
+      .then(res => res.json())
+      .then(res => res[Math.floor(Math.random() * res.length)])
+      .then(res => this.setState(() => ({ queen: res })));
+  };
+
+  componentDidMount() {
+    this.getRandomQueen();
+  }
+
+
+  render() {
+    return (
+      <>
+        <Button onClick={this.getRandomQueen} />
+        <Result queen={ this.state.queen }/>
+      </>
+    );
+  }
 }
+
+// export default function App() {
+
+//   return (
+//     <>
+//       <Button onClick={getRandomQueen} />
+//       <Result />
+//     </>
+//   );
+// }
