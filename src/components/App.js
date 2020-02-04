ed9aa94cfa69379dae679cad54bca9c85f7005bc
Joseph Tatum <josephtatum@gmail.com>
2020-02-03 16:14:42 -0800

    wrote function version of app without useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import GetQueen from '../containers/GetQueen';
 import '../styles.css';
+import GetQueenFn from '../containers/GetQueenFn';
 
 export default function App() {
 
   return (
     <>
-      <GetQueen />
+      <GetQueenFn />
     </>
   );
 }
---
+++ src/containers/GetQueenFn.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Button from '../components/Button';
+import Result from '../components/Result';
+import { getRandomQueen } from '../services/getQueens';
+
+
+const GetQueenFn = () => {
+
+  const [queen, setQueen] = useState({});
+
+  const handleClick = () => {
+    return getRandomQueen()
+      .then(res => {
+        setQueen(res);
+      });
+  };
+
+  return (
+    <>
+      <Button onClick={handleClick} />
+      <Result queen={queen} />
+    </>
+  );
+};
+
+export default GetQueenFn;
+
+// export default class GetQueen extends Component {
+
+//   state = {
+//     queen: {}
+//   }
+
+  // getQueen = () => {
+  //   return getRandomQueen().then(res => this.setState(() => ({ queen: res })));
+  // };
+
+//   componentDidMount() {
+//     this.getQueen();
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Button onClick={this.getQueen} />
+//         <Result queen={this.state.queen} />
+//       </>
+//     );
+//   }
+// }
